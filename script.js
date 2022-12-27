5b7c407539c0b1b59cbef995eebffbd110e47f4a
Quinten <116814645+zqrup@users.noreply.github.com>
2022-12-27 18:22:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ navigator.geolocation.getCurrentPosition(getWeatherData);
 function getWeatherData(position){
   let userLocation = "lat=" + position.coords.latitude + "&" + "lon=" + position.coords.longitude;
   $.getJSON(
-    "https://api.openweathermap.org/data/2.5/weather?" + userLocation + "&units=metric&appid=d1b42716cdb097a4a13abdc36c715e8a", 
+    "https://api.openweathermap.org/data/2.5/weather?" + userLocation + "&units=metric&appid=key", 
     function(data){
 
       var temp = Math.round(data.main.temp);
